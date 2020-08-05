c30e8428c5b54f5482fd738d53a688288abefe9e
davide-amabile <davideamabile81@gmail.com>
2020-08-05 16:40:03 +0200

    secondo snack

--- snack2/js/script.js
+++ snack2/js/script.js
@@ -1,1 +1,12 @@
-alert();
+// inserire le 2 parole
+var word1 = prompt("Inserire la prima parola");
+var word2 = prompt("Inserire la seconda parola");
+
+// stampare la parola più lunga
+
+if ( word1.length > word2.length) {
+  document.getElementById('parola').innerHTML = ("la parola più lunga è:" + " " + word1);
+} else if ( word2.length > word1.length ) {
+  document.getElementById('parola').innerHTML = ("la parola più lunga è:" + " " + word2);
+} else if ( word1.length === word2.length )
+  document.getElementById('parola').innerHTML = ("Le parole sono lunghe uguali")
