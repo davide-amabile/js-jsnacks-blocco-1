21ee9542a53c9753e4934c1f422a4cb5e6471c7b
davide-amabile <davideamabile81@gmail.com>
2020-08-05 16:05:05 +0200

    primo snack

---
+++ snack1/js/script.js
@@ -0,0 +1,11 @@
+// chiedere i 2 numeri all'utente
+
+var num1 = parseInt(prompt("Inserire il primo numero"));
+var num2 = parseInt(prompt("Inserire il secondo numero"));
+
+if ( num1 > num2 ) {
+  document.getElementById('number').innerHTML = ("Il numero maggiore è" + " " + num1);
+} else if (num2 > num1) {
+  document.getElementById('number').innerHTML = ("Il numero maggiore è" + " " + num2);
+} else if  ( num1 === num2)
+  document.getElementById('number').innerHTML = ("I numeri sono uguali");
