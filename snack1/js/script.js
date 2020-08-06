1ada5472bd49555d7a03c31b42be6106525d282b
davide-amabile <davideamabile81@gmail.com>
2020-08-06 09:15:46 +0200

    aggiunto is NaN

--- snack1/js/script.js
+++ snack1/js/script.js
@@ -3,9 +3,14 @@
 var num1 = parseInt(prompt("Inserire il primo numero"));
 var num2 = parseInt(prompt("Inserire il secondo numero"));
 
-if ( num1 > num2 ) {
+
+if ( isNaN(num1) || isNaN(num2) ) {
+  document.getElementById('number').innerHTML = ("Inserire numero");
+}
+else if ( num1 > num2 ) {
   document.getElementById('number').innerHTML = ("Il numero maggiore è" + " " + num1);
 } else if (num2 > num1) {
   document.getElementById('number').innerHTML = ("Il numero maggiore è" + " " + num2);
-} else if  ( num1 === num2)
+} else if  ( num1 === num2) {
   document.getElementById('number').innerHTML = ("I numeri sono uguali");
+}
--- snack3/js/script.js
+++ snack3/js/script.js
@@ -3,7 +3,11 @@ var num = parseInt(prompt("Inserire il numero"));
 
 // stampasre il numero se è pari se no stampare il successivo
 
-if ( num % 2 == 0 ) {
+
+if (isNaN(num)) {
+  document.getElementById('number').innerHTML = ("Inserire un numero");
+}
+else if ( num % 2 == 0 ) {
   document.getElementById('number').innerHTML = ( num );
 } else if ( num % 2 == 1 ) {
   document.getElementById('number').innerHTML = ( num + 1 );
